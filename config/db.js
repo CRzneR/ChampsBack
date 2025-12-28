c0f83678c2fbfe52d37b895ab8b1bc9512bd402d
CRzneR <renzchris@web.de>
2025-12-29 00:05:50 +0100

    changed uir

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const uri = process.env.DB_URI;
+  const uri = process.env.MONGODB_URI;
 
   if (!uri) {
     console.error("❌ DB_URI ist nicht gesetzt (.env fehlt?)");
